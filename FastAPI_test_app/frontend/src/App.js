7e5897d3fb47a7f34d54418bdea2db0df26bffea
Biruk Tafesse <98220455+biruk-tafese@users.noreply.github.com>
2024-05-19 00:58:05 +0000

    google auth

--- FastAPI_test_app/frontend/src/App.js
+++ FastAPI_test_app/frontend/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (response) => {
+    if (response.tokenId){
+      fetch('http://localhost:8000/auth?token='+ response.tokenId,{
+        credentials: 'include',
+        // To cause browsers to send a request with credentials included on both same-origin and cross-origin calls, 
+        // add credentials: 'include' to the init object you pass to the fetch() method.
+       })
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        alert(myJson)
+      });
+    }
+}
+
+const temp = () =>{
+  fetch('http://localhost:8000',{
+    credentials:'include' 
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    alert(myJson)
+  });
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <GoogleLogin
+          clientId="116988534719-0r0.apps.googleusercontent.com"
+          buttonText="Google Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+        <br/>
+        <button onClick={temp}> Check session </button>
     </div>
   );
 }
